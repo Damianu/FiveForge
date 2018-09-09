896e1e676fb04951d48d5a2d408f3cb3bddece46
Damianu <xdamianu@gmail.com>
2018-09-10 01:51:57 +0200

    Added temporary way of setting images on monsters

--- src/fforge_monster.js
+++ src/fforge_monster.js
@@ -7,6 +7,32 @@ class FFMonster extends FFActor
     {
         return "monster";
     }
+    render(app, ...args)
+    {
+        let r = super.render(app, ...args);
+        let setimg = r.find("#set-image");
+        let obj = this._obj;
+        let data = this._data;
+        setimg.click(function(){
+            var imgList = sync.render("ui_filePicker")(this._obj, app, {
+                filter : "img",
+                change : function(ev, ui, val){
+                    data.info.img.current = val;
+                    obj.sync("updateAsset");
+                    layout.coverlay("icons-picker");
+                }
+              });
+              var pop = ui_popOut({
+                target : app,
+                prompt : true,
+                id : "icons-picker",
+                align : "top",
+                style : {"width" : assetTypes["filePicker"].width, "height" : assetTypes["filePicker"].height}
+            }, imgList);
+        })
+        setimg.attr("id","")
+        return r;
+    }
 }
 
 FFMonster.setupType("Monster");
